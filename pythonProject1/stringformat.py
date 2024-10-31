7f0e8e78503b6fbd483b6ccdb517880ca25f484d
namvfg <duakhongchau1234@gmail.com>
2024-10-31 21:58:34 +0700

    test rat nhieu thu

--- pythonProject1/stringformat.py
+++ pythonProject1/stringformat.py
@@ -1,6 +1,15 @@
-a = int(input("Nhap a: "))
-b = int(input("Nhap b: "))
+#a = int(input("Nhap a: "))
+#b = int(input("Nhap b: "))
 
-print(str.format("{2} = {0} - {1}", a, b, a - b))
-print(str.format("{re} = {a} - {b}", a = a, b = b,re = a - b))
-print(str.format("{re} = {0} - {1}",a, b,re = a - b))
+#print(str.format("{2} = {0} - {1}", a, b, a - b))
+#print(str.format("{re} = {a} - {b}", a = a, b = b,re = a - b))
+#print(str.format("{re} = {0} - {1}",a, b,re = a - b))
+
+str = "hhhhh nguyen Dong Dun 12A1 hhh"
+print(str.center(32, "r"))
+print(str.count("n"))
+#print(str.capitalize())
+print(str.strip("h"))
+print(str[str.find("Do"):])
+print(str.replace("Dong", "Nam"))
+print(str.split())
