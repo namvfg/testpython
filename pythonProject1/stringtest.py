c7ddf97a7fd070646ef7d405538c766b9507665a
namvfg <duakhongchau1234@gmail.com>
2024-10-31 21:18:27 +0700

    test moi ban oi

--- pythonProject1/stringtest.py
+++ pythonProject1/stringtest.py
@@ -6,3 +6,5 @@ print(f"Ky tu thu {i}: {str[i]}")
 print(f"Chuoi tu i den j: {str[i:j]}")
 print(f"Chuoi tu 0 den j: {str[:j]}")
 print(f"Chuoi tu i den cuoi: {str[i:]}")
+j = -5;
+print(f"Chuoi tu i den j: {str[i:j]}")
